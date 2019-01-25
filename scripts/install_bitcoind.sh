bd19ea31b8b8bc8bde3119bc79c82b63edb09c69
Devin Bileck <603793+devinbileck@users.noreply.github.com>
2019-01-25 15:47:27 -0800

    Fixes

--- scripts/install_bitcoind.sh
+++ scripts/install_bitcoind.sh
@@ -7,10 +7,14 @@ cd ~/
 wget https://bitcoin.org/bin/bitcoin-core-$VERSION/bitcoin-$VERSION-x86_64-linux-gnu.tar.gz
 tar -xvf bitcoin-$VERSION-x86_64-linux-gnu.tar.gz
 rm bitcoin-$VERSION-x86_64-linux-gnu.tar.gz
-cp -f bitcoin-$VERSION/bin/* /usr/local/bin
+sudo cp -f bitcoin-$VERSION/bin/* /usr/local/bin
 rm -fr bitcoin-$VERSION
 
-echo <<< EOL
+if [ ! -d ~/.bitcoin ]; then
+    mkdir ~/.bitcoin
+fi
+
+cat >~/.bitcoin/bitcoin.conf <<EOL
 regtest=1
 rpcport=18443
 server=1
@@ -18,9 +22,11 @@ txindex=1
 rpcuser=bitcoin
 rpcpassword=$RPC_PASSWORD
 blocknotify=bash ~/.bitcoin/blocknotify %s
-EOL > ~/.bitcoin/bitcoin.conf;
+EOL
 
-echo <<< EOL
+cat >~/.bitcoin/blocknotify <<EOL
 #!/usr/bin/env bash
 echo $1 | nc -w 1 127.0.0.1 5120
-EOL > ~/.bitcoin/blocknotify
+EOL
+
+chmod +x ~/.bitcoin/blocknotify
